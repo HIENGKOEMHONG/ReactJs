6c9678984425378a9122d3bcc4a18abd5161df2e
HIENGKOEMHONG <mrkhong70@gmail.com>
2024-12-24 16:44:13 +0700

    He commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import Click from './Click.jsx'
 import ClickParameter from './ClickParameter.jsx'
 import Change from './Change.jsx'
 
-import index from './src/index.js';
+
+
+
 
 createRoot(document.getElementById('click')).render(<Click />)
 createRoot(document.getElementById('click-parameter')).render(<ClickParameter />)
